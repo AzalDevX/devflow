340d99bebc40b640b06cc145b3ae6ebf8b9e0d5d
Gonzalo Azaldegi <98758892+AzalDevX@users.noreply.github.com>
2025-01-28 00:59:41 +0100

    feat: API y funcionalidades adicionales

---
+++ src/pages/api/posts.json.ts
@@ -0,0 +1,23 @@
+import { getCollection } from "astro:content"
+
+export async function GET() {
+  const posts = await getCollection("blog")
+  return new Response(
+    JSON.stringify(
+      posts.map((post) => ({
+        slug: post.slug,
+        data: {
+          title: post.data.title,
+          description: post.data.description,
+          publishDate: post.data.publishDate,
+        },
+      })),
+    ),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  )
+}
+
